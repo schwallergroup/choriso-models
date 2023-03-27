86de5e692b903d47ab772e1d388296fa403d3ed3
malte.franke <iz782675@login18-g-1.hpc.itc.rwth-aachen.de>
2023-03-27 20:44:57 +0200

    fix huggingface

--- benchmark_models.py
+++ benchmark_models.py
@@ -7,6 +7,7 @@ import json
 import numpy as np
 import tempfile
 import datetime
+from sklearn.metrics import top_k_accuracy_score
 from tokenizers import Regex, Tokenizer, pre_tokenizers, processors
 from tokenizers import models as tokenizer_models
 from tokenizers.trainers import WordLevelTrainer
@@ -89,6 +90,7 @@ class HuggingFaceTransformer(ReactionModel):
 
             chem_tokenizer.pre_tokenizer = pre_tokenizers.Split(pattern, "isolated")
             chem_tokenizer.post_processor = processors.BertProcessing(cls=("[CLS]", 1), sep=("[SEP]", 2))
+
             trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[CLS]", "[SEP]", "[MASK]"])
 
             # get data for tokenizer training --> for building the vocab!
@@ -113,13 +115,17 @@ class HuggingFaceTransformer(ReactionModel):
                                                          cls_token="[CLS]",
                                                          sep_token="[SEP]",
                                                          mask_token="[MASK]",
-                                                         model_max_length=1024)
+                                                         model_max_length=2048,
+                                                         padding_side="right")
 
                 chem_tokenizer.save_pretrained(chem_tokenizer_path)
 
         else:
             # use pre-trained tokenizer
             chem_tokenizer = AutoTokenizer.from_pretrained(chem_tokenizer_path)
+
+        chem_tokenizer.bos_token = chem_tokenizer.cls_token
+        chem_tokenizer.eos_token = chem_tokenizer.sep_token
 
         self.tokenizer = chem_tokenizer
 
@@ -140,11 +146,15 @@ class HuggingFaceTransformer(ReactionModel):
                 enc_config = model_kwargs["encoder"]
                 dec_config = model_kwargs["decoder"]
 
-                config = EncoderDecoderConfig.from_encoder_decoder_configs(enc_config, dec_config)
+                model_kwargs.pop("encoder", None)
+                model_kwargs.pop("decoder", None)
+
+                config = EncoderDecoderConfig.from_encoder_decoder_configs(enc_config, dec_config, **model_kwargs)
 
                 # overwrite arguments with tokenizer specifications
                 config.decoder_start_token_id = self.tokenizer.cls_token_id
                 config.pad_token_id = self.tokenizer.pad_token_id
+                config.eos_token_id = self.tokenizer.eos_token_id
                 config.vocab_size = self.tokenizer.vocab_size
 
                 config.tie_word_embeddings = True
@@ -162,6 +172,9 @@ class HuggingFaceTransformer(ReactionModel):
         # resize the token length to fit the tokenizer
         self.model.encoder.resize_token_embeddings(len(self.tokenizer))
         self.model.decoder.resize_token_embeddings(len(self.tokenizer))
+
+        # print("Model output: ", self.model(*tuple(test_encoded.values()))[0].shape)
+        # breakpoint()
 
         # route the given dir to the model dir
         train_args["output_dir"] = os.path.join(self.model_dir, train_args["output_dir"])
@@ -188,18 +201,23 @@ class HuggingFaceTransformer(ReactionModel):
 
             # else do preprocessing and save it in a json file so it can be retrieved more easily
             else:
+                # read and split data
                 reactions = pd.read_csv(os.path.join(data_dir, f"{file_name}.tsv"), sep="\t")
-
                 split_reactions = prepare_data(reactions, rsmiles_col="canonic_rxn")
-
                 split_reactions = split_reactions.to_dict("list")
 
                 # tokenize the dataset
-                reaction_dataset = self.tokenizer(split_reactions["reactants"], text_target=split_reactions["products"],
-                                               truncation=True, padding=True, max_length=1000)
+                reaction_dataset = self.tokenizer(split_reactions["reactants"][:10000],
+                                                  text_target=split_reactions["products"][:1000],
+                                                  truncation=True, padding=True)  #, max_length=1000)
 
                 # make it a dict
                 reaction_dataset = dict(reaction_dataset)
+
+                labels = np.array(reaction_dataset["labels"])
+                labels = np.where(labels != self.tokenizer.pad_token_id, labels, -100)
+
+                reaction_dataset["labels"] = labels.tolist()
 
                 # save results
                 with open(save_path, "w") as outfile:
@@ -226,33 +244,95 @@ class HuggingFaceTransformer(ReactionModel):
         result = {"accuracy": result}
         return result
 
+    def tokenwise_acc(self, labels: np.array, preds: np.array):
+        # taken from the onmt docs
+        """non_padding = labels.ne(self.tokenizer.pad_token_id)
+        num_correct = preds.eq(labels).masked_select(non_padding).sum().item()
+        num_non_padding = non_padding.sum().item()"""
+
+        non_padding = np.not_equal(labels, self.tokenizer.pad_token_id)
+        print("non_padding shape: ", non_padding.shape)
+        labels_pad_mask = labels[non_padding]
+        print("labels_pad_mask shape: ", labels_pad_mask.shape)
+        correct_matrix = np.equal(preds, labels)
+        print("correct_matrix shape: ", correct_matrix.shape)
+        correct_matrix_pad_mask = correct_matrix[non_padding]
+        print("correct_matrix_pad_mask shape: ", correct_matrix_pad_mask.shape)
+        num_correct = correct_matrix_pad_mask.flatten().sum().item()
+        num_non_padding = non_padding.flatten().sum().item()
+
+        accuracy = 100 * (num_correct / num_non_padding)
+        return accuracy
+
+    def top_k_acc(self, labels: np.array, preds: np.array, k=5):
+        assert k > 0 and type(k) == int, "k has to be an integer > 0"
+
+        top_k_accs = {f"top_{i}_acc": 0 for i in range(1, k+1)}
+
+        for i in range(1, k+1):
+            top_k_accs[f"top_{i}_acc"] = top_k_accuracy_score(labels, preds, k=i)
+
+        return top_k_accs
+
     def compute_metrics(self, eval_preds):
         labels = eval_preds.label_ids
         preds = eval_preds.predictions
-        # transform back to SMILES
-        decoded_preds = self.tokenizer.batch_decode(preds, skip_special_tokens=True)
-        print(decoded_preds)
 
-        # Replace -100 in the labels as we can't decode them.
-        labels = np.where(labels != -100, labels, self.tokenizer.pad_token_id)
-        decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
+        labels[labels == -100] = self.tokenizer.pad_token_id
 
-        # canonicalize smiles
-        decoded_preds, decoded_labels = list(map(self.postprocess_text, decoded_preds)), list(
-            map(self.postprocess_text, decoded_labels))
-        # apply metric
-        result = self.apply_metric(decoded_preds, decoded_labels)
+        print("Predictions: ", preds)
+        print("Labels: ", labels)
+        print("Predictions shape: ", preds.shape)
+        print("Labels shape: ", labels.shape)
+
+        # accuracy = self.tokenwise_acc(labels, preds)
+
+        # accuracy = evaluate.load("accuracy")
+        # result = accuracy.compute(references=labels, predictions=preds)
+        # result = {"accuracy": accuracy}
+
+        def remove_special_tokens(line):
+            mask = np.isin(line, self.tokenizer.all_special_ids)
+            line = line[~mask]
+            return line
+
+        accuracy = evaluate.load("accuracy")
+        sum = 0
+        for p, l in zip(preds, labels):
+            print(f"prediction: {self.tokenizer.decode(p, skip_special_tokens=True)}\n label: {self.tokenizer.decode(l, skip_special_tokens=True)}\n")
+            l = remove_special_tokens(l)
+            p = remove_special_tokens(p)
+
+            if len(l) > len(p):
+                # pad p
+                p = np.pad(p, (0, int(len(l)-len(p))), "constant")
+                size = len(l)
+            elif len(l) < len(p):
+                # pad l
+                l = np.pad(l, (0, int(len(p)-len(l))), "constant")
+                size = len(p)
+            else:
+                # same length, don't pad
+                size = len(l)
+            acc = accuracy.compute(references=l, predictions=p)["accuracy"]
+            print(f"prediction: {p}\n label: {l}\n")
+            print("accuracy: ", acc)
+            sum += acc
+
+        result = {"accuracy": (sum / preds.shape[0])}
+
         return result
 
     def train(self):
         """Train the reaction model. Should also contain validation and test steps"""
         data_collator = DataCollatorForSeq2Seq(tokenizer=self.tokenizer, model=self.model)
+
         trainer = Seq2SeqTrainer(model=self.model,
                                  args=self.train_args,
                                  train_dataset=self.train_dataset,
                                  eval_dataset=self.val_dataset,
                                  data_collator=data_collator,
-                                 tokenizer=self.tokenizer,
+                                 # tokenizer=self.tokenizer,
                                  compute_metrics=self.compute_metrics,
                                  callbacks=[WandbCallback, CodeCarbonCallback])
         trainer.train()
@@ -325,7 +405,7 @@ if __name__ == "__main__":
         # training setup
         "max_steps": 200000,  # total number of training steps
         "evaluation_strategy": "steps",
-        "eval_steps": 20,
+        "eval_steps": 500,
         "save_strategy": "steps",
         "save_steps": 5000,
 
@@ -333,7 +413,7 @@ if __name__ == "__main__":
         "learning_rate": 1.5e-3,
         "save_total_limit": 3,
         "per_device_train_batch_size": 32,  # batch size per device during training
-        "per_device_eval_batch_size": 32,  # batch size for evaluation
+        "per_device_eval_batch_size": 64,  # batch size for evaluation
         "warmup_steps": 8000,  # number of warmup steps for learning rate scheduler
         "weight_decay": 0.01,  # strength of weight decay
         "logging_dir": 'logs',  # directory for storing logs
@@ -345,25 +425,29 @@ if __name__ == "__main__":
         "gradient_accumulation_steps": 4,
         "eval_accumulation_steps": 10,
         "predict_with_generate": True,
+        "dataloader_num_workers": 4,
         # "lr_scheduler_type": "noam"
         # "load_best_model_at_end": False,
     }
     enc_config = BertConfig(hidden_size = 384,
-                              num_hidden_layers = 4,
-                              num_attention_heads = 8,
-                              intermediate_size = 2048,
-                              hidden_act = 'gelu',
-                              hidden_dropout_prob = 0.1,
-                              attention_probs_dropout_prob = 0.1,
-                              max_position_embeddings = 512,
-                              type_vocab_size = 2,
-                              initializer_range = 0.02,
-                              layer_norm_eps = 1e-12,
-                              pad_token_id = 0,
-                              position_embedding_type = 'absolute',
-                              use_cache = True,
-                              classifier_dropout = None,
-                            num_beams=10, vocab_size=1024)
+                            num_hidden_layers = 4,
+                            num_attention_heads = 8,
+                            intermediate_size = 2048,
+                            hidden_act = 'gelu',
+                            hidden_dropout_prob = 0.1,
+                            attention_probs_dropout_prob = 0.1,
+                            max_position_embeddings = 512,
+                            type_vocab_size = 2,
+                            initializer_range = 0.02,
+                            layer_norm_eps = 1e-12,
+                            position_embedding_type = 'absolute',
+                            use_cache = True,
+                            classifier_dropout = None,
+                            num_beams=10,
+                            vocab_size=1024,
+                            max_length=512,
+                            min_length=1
+                            )
 
     dec_config = BertConfig(hidden_size=384,
                             num_hidden_layers=4,
@@ -376,14 +460,20 @@ if __name__ == "__main__":
                             type_vocab_size=2,
                             initializer_range=0.02,
                             layer_norm_eps=1e-12,
-                            pad_token_id=0,
                             position_embedding_type='absolute',
                             use_cache=True,
-                            classifier_dropout=None, num_beams=10, vocab_size=1024)
+                            classifier_dropout=None,
+                            num_beams=10,
+                            vocab_size=1024,
+                            max_length=512,
+                            min_length=1)
 
     model_args = {
         "encoder": enc_config,
-        "decoder": dec_config
+        "decoder": dec_config,
+        "length_penalty": 2.0,
+        "max_length": 96,
+        "min_length": 3
     }
     reaction_model = HuggingFaceTransformer(model_architecture="bert-base-uncased",
                                                   train_args=train_args, model_args=model_args)
