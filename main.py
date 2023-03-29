2290daf61ebed617b31361bd8e6dec98c0d841a8
malte.franke <malte.franke1@gmx.de>
2023-03-29 19:00:47 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+from benchmark_models import *
+
+
+def main():
+    # instantiate model depending on args
+
+    """
+    if args.model = G2S:
+        reaction_model = G2S()
+    elif args.model = ONMT:
+        reaction_model = OpenNMT()
+    elif args.model = HFTransformer:
+        reaction_model = HuggingFaceTransformer()
+    else:
+        raise NotImplementedError()
+    """
+
+    # instantiate the pipeline with the model
+    """
+    pipeline = BenchmarkPipeline(model=reaction_model)
+    """
+
+    # call pipeline function based on args
+    """
+    
+    """
