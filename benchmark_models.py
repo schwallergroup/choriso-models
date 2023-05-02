254070b1b0bbbc2d665e2091cf573f4fe469b56f
malte.franke <malte.franke1@gmx.de>
2023-05-02 12:56:42 +0200

    add eco2ai

--- benchmark_models.py
+++ benchmark_models.py
@@ -1,3 +1,4 @@
+import eco2ai
 from reaction_model import *
 
 
@@ -7,16 +8,37 @@ class BenchmarkPipeline:
         self.model = model
 
     def run_train_pipeline(self, dataset):
+        preprocess_tracker = eco2ai.Tracker(
+            project_name=f"{self.model.name}_benchmark",
+            experiment_description=f"preprocess {self.model.name} model",
+            file_name=f"{self.model.model_dir}/{dataset}/preprocess_emission.csv"
+        )
+        preprocess_tracker.start()
         self.model.preprocess(dataset=dataset)
+        preprocess_tracker.stop()
+
+        train_tracker = eco2ai.Tracker(
+            project_name=f"{self.model.name}_benchmark",
+            experiment_description=f"train {self.model.name} model",
+            file_name=f"{self.model.model_dir}/{dataset}/train_emission.csv"
+        )
+        train_tracker.start()
         self.model.train(dataset=dataset)
+        train_tracker.stop()
 
     def predict(self, dataset):
+        predict_tracker = eco2ai.Tracker(
+            project_name=f"{self.model.name}_benchmark",
+            experiment_description=f"predict {self.model.name} model",
+            file_name=f"{self.model.model_dir}/{dataset}/predict_emission.csv"
+        )
+        predict_tracker.start()
         self.model.predict(dataset=dataset)
+        predict_tracker.stop()
 
     def run_pipeline(self, dataset):
-        self.model.preprocess(dataset=dataset)
-        self.model.train(dataset=dataset)
-        self.model.predict(dataset=dataset)
+        self.run_train_pipeline(dataset=dataset)
+        self.predict(dataset=dataset)
 
     def run_mode_from_args(self, args):
         if args.mode == "t":
