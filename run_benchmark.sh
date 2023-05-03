2efe641c41f9982d148845e5c2305e59fa17c203
Malte Franke <franke@izar.ib0.cm.izar.local>
2023-05-03 14:13:00 +0200

    remove uspto

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-#SBATCH --job-name=myjob
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --time=48:00:00
-
 models=(G2S ONMT)
 datasets=(cjhif choriso_low_mw choriso_high_mw)
 
