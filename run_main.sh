e50d7a39229db0aafe5d64d08733e834f1054d1d
maltefranke <65611948+maltefranke@users.noreply.github.com>
2023-07-20 13:42:07 +0200

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=50G
+#SBATCH --mem-per-cpu=80G
 
 #request one gpu
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 
-#SBATCH --time=0-96:00:00
+#SBATCH --time=0-72:00:00
 #SBATCH --mail-type=ALL
 
 ### begin of executable commands
 module load cuda
 module load intel
 
-export PATH=~/anaconda3/envs/reaction_prediction/bin:$PATH
+export PATH=~/anaconda3/envs/choriso-models/bin:$PATH
 # conda env
-source activate reaction_prediction
+source activate choriso-models
 
-export PYTHONPATH=$PYTHONPATH:~/rxn_predict
+export PYTHONPATH=$PYTHONPATH:~/choriso-models
 
 
 python main.py -model $model -p tp -ds $dataset
